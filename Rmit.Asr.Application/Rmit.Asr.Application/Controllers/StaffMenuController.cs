886070fb4ae6871cd7256849aa875b386eb60983
shawnotaylor <shawnotaylor04@gmail.com>
2019-01-22 22:42:31 +1100

    added staff menu template

---
+++ Rmit.Asr.Application/Rmit.Asr.Application/Controllers/StaffMenuController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Rmit.Asr.Application.Controllers
+{
+    public class StaffMenuController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
