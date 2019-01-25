00e461d6aa6876bba95cc30f65babb8eeaee5baa
shawnotaylor <shawnotaylor04@gmail.com>
2019-01-25 14:27:23 +1100

    create slot validation rules added server side

--- Rmit.Asr.Application/Rmit.Asr.Application/Controllers/StaffMenuController.cs
+++ Rmit.Asr.Application/Rmit.Asr.Application/Controllers/StaffMenuController.cs
@@ -40,12 +40,54 @@ namespace Rmit.Asr.Application.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> CreateSlot([Bind("RoomID,StartTime,StaffID")] Slot slot)
         {
+            if (!ModelState.IsValid) return View(slot);
+
+            if (!_context.Room.Any(r => r.RoomID == slot.RoomID))
+            {
+                ModelState.AddModelError("RoomID", $"Room {slot.RoomID} does not exist.");
+            }
+
+            if (!_context.Staff.Any(r => r.StaffID == slot.StaffID))
+            {
+                ModelState.AddModelError("StaffID", $"Staff {slot.StaffID} does not exist.");
+            }
+
+            if (GetAvailableRooms(slot.StartTime).All(r => r.RoomID != slot.RoomID))
+            {
+                ModelState.AddModelError("RoomID", $"Room {slot.RoomID} has reached a maximum booking of 2 per day.");
+            }
+
+            var staffSlotCount = _context.Slot.Count(s => s.StartTime.Date == slot.StartTime.Date && s.StaffID == slot.StaffID);
+            if(staffSlotCount >= 4)
+            {
+                ModelState.AddModelError("StartTime", $"Staff {slot.StaffID} has a maxmimum of 4 bookings at {slot.StartTime:dd-MM-yyyy}.");
+            }
+            var staffSlotExists = _context.Slot.FirstOrDefault(s => s.RoomID == slot.RoomID && s.StartTime == slot.StartTime && s.StaffID != slot.StaffID);
+            if (staffSlotExists != null)
+            {
+                ModelState.AddModelError("StaffID", $"Slot for staff {staffSlotExists.StaffID} at room {slot.RoomID} {slot.StartTime:dd-MM-yyyy H:mm} already exists.");
+            }
+
+            var slotExist = _context.Slot.Any(x => x.RoomID == slot.RoomID && x.StartTime == slot.StartTime);
+            if (slotExist)
+            {
+                ModelState.AddModelError("StaffID", $"Slot already exists.");
+            }
+
+            var staffSlot = _context.Slot.FirstOrDefault(x => x.StaffID == slot.StaffID && x.StartTime == slot.StartTime);
+            if (staffSlot != null)
+            {
+                ModelState.AddModelError("StaffID", $"Staff {staffSlot.StaffID} has already been booked at room {staffSlot.RoomID} {staffSlot.StartTime:dd-MM-yyyy H:mm}.");
+            }
+
             if (ModelState.IsValid)
             {
                 _context.Add(slot);
                 await _context.SaveChangesAsync();
+
                 return RedirectToAction("Index");
             }
+
             return View(slot);
         }
 
@@ -72,34 +114,34 @@ namespace Rmit.Asr.Application.Controllers
         [HttpGet]
         public IActionResult RoomsAvail(DateTime day)
         {
+
             if (!ModelState.IsValid) return View();
             
+            IQueryable<Room> rooms = GetAvailableRooms(day);
+
+            return View(rooms);
+        }
+
+        private IQueryable<Room> GetAvailableRooms(DateTime date)
+        {
             // Get unavailable rooms
             IQueryable<Room> unavailableRooms = _context.Room
                 .Include(r => r.Slots)
-                .Where(r => r.Slots.Any(s => s.StartTime.Date == day.Date) && r.Slots.Count >= 2);
+                .Where(r => r.Slots.Any(s => s.StartTime.Date == date.Date) && r.Slots.Count >= 2);
 
             // Compare unavailable rooms and exclude
             IQueryable<Room> rooms = _context.Room
                 .Where(r => unavailableRooms.All(x => x.RoomID != r.RoomID));
-                
-            return View(rooms);
+
+            return rooms;
         }
-       
-        //public async Task<IActionResult> RemoveSlotAsync(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    var slot = await _context.Slots.FirstOrDefaultAsync(x => x.RoomID == id );
-        //    if (slot == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(slot);
-        //    return View();
-        //}
+
+
+        public async Task<IActionResult> RemoveSlot()
+        {
+
+            return View();
+        }
 
 
 
