115e804ee21b912de0c1594a6623dc4ffbf70761
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-01-26 09:47:58 +1100

    Make sure student and staff are not logging into each other.

--- Rmit.Asr.Application/Rmit.Asr.Application/Areas/Identity/Pages/Student/ExternalLogin.cshtml.cs
+++ Rmit.Asr.Application/Rmit.Asr.Application/Areas/Identity/Pages/Student/ExternalLogin.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
@@ -6,7 +7,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Rmit.Asr.Application.Data;
 using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace Rmit.Asr.Application.Areas.Identity.Pages.Student
@@ -17,12 +20,15 @@ namespace Rmit.Asr.Application.Areas.Identity.Pages.Student
         private readonly SignInManager<Models.Student> _signInManager;
         private readonly UserManager<Models.Student> _userManager;
         private readonly ILogger<StudentExternalLoginModel> _logger;
+        private ApplicationDataContext _context;
 
         public StudentExternalLoginModel(
+            ApplicationDataContext context,
             SignInManager<Models.Student> signInManager,
             UserManager<Models.Student> userManager,
             ILogger<StudentExternalLoginModel> logger)
         {
+            _context = context;
             _signInManager = signInManager;
             _userManager = userManager;
             _logger = logger;
@@ -61,6 +67,7 @@ namespace Rmit.Asr.Application.Areas.Identity.Pages.Student
 
         public async Task<IActionResult> OnGetCallbackAsync(string returnUrl = null, string remoteError = null)
         {
+            // TODO: Move student and staff Razer classes into one
             returnUrl = returnUrl ?? Url.Content("~/");
             
             if (remoteError != null)
@@ -74,9 +81,26 @@ namespace Rmit.Asr.Application.Areas.Identity.Pages.Student
             if (info == null)
             {
                 ErrorMessage = "Error loading external login information.";
+                
                 return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
             }
-
+            
+            // Get the external login email and check if it already exists
+            if (info.Principal.HasClaim(c => c.Type == ClaimTypes.Email))
+            {
+                Input = new InputModel
+                {
+                    Email = info.Principal.FindFirstValue(ClaimTypes.Email)
+                };
+                
+                if (await _context.Staff.AnyAsync(u => u.Email == Input.Email))
+                {
+                    ErrorMessage = "User email has already been registered by a staff.";
+                    
+                    return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
+                }
+            }
+            
             // Sign in the user with this external login provider if the user already has a login.
             SignInResult result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, false, true);
             
@@ -91,19 +115,10 @@ namespace Rmit.Asr.Application.Areas.Identity.Pages.Student
             {
                 return RedirectToPage("./Lockout");
             }
-
-            // If the user does not have an account, then ask the user to create an account.
+            
             ReturnUrl = returnUrl;
             LoginProvider = info.LoginProvider;
-            
-            if (info.Principal.HasClaim(c => c.Type == ClaimTypes.Email))
-            {
-                Input = new InputModel
-                {
-                    Email = info.Principal.FindFirstValue(ClaimTypes.Email)
-                };
-            }
-            
+
             return Page();
         }
 
@@ -117,9 +132,17 @@ namespace Rmit.Asr.Application.Areas.Identity.Pages.Student
             if (info == null)
             {
                 ErrorMessage = "Error loading external login information during confirmation.";
+                
                 return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
             }
 
+            if (_context.Staff.Any(u => u.Email == Input.Email))
+            {
+                ErrorMessage = "User email has already been registered as a staff.";
+                
+                return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
+            }
+            
             if (ModelState.IsValid)
             {
                 var user = new Models.Student { FirstName = Input.FirstName, LastName = Input.LastName,  UserName = Input.Email, Email = Input.Email };
