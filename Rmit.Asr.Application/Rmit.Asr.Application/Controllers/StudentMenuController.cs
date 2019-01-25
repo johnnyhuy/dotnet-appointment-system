5a65ef3242a5a8b9af5147f85d540402151873e7
shawnotaylor <shawnotaylor04@gmail.com>
2019-01-26 00:05:14 +1100

    added staff availablitity

--- Rmit.Asr.Application/Rmit.Asr.Application/Controllers/StudentMenuController.cs
+++ Rmit.Asr.Application/Rmit.Asr.Application/Controllers/StudentMenuController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Rmit.Asr.Application.Data;
+using Rmit.Asr.Application.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -29,6 +30,28 @@ namespace Rmit.Asr.Application.Controllers
         {
             return View(_context.Student.ToList() );
         }
+
+
+        [HttpGet]
+        public IActionResult StaffAvail(DateTime day)
+        {
+
+            if (!ModelState.IsValid) return View();
+
+            // gets all slots for that day that dont have a student booked into it
+            // we know that staff cannot create a new slot if they have reached thier max bookings for that day
+            // so all these slots must mean that these staff members are available
+            var availStaff = _context.Slot.Where(x => x.StartTime.Date == day.Date && x.StudentID == null);
+
+
+            return View(availStaff);
+
+
+
+        }
+
+
+
 
     }
 }
