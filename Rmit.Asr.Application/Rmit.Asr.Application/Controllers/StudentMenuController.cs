a1b8adea456980bc5b40343ba6be442f484a59bb
shawnotaylor <shawnotaylor04@gmail.com>
2019-01-25 23:07:46 +1100

    added ListStudent function

--- Rmit.Asr.Application/Rmit.Asr.Application/Controllers/StudentMenuController.cs
+++ Rmit.Asr.Application/Rmit.Asr.Application/Controllers/StudentMenuController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Rmit.Asr.Application.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,24 @@ namespace Rmit.Asr.Application.Controllers
 {
     public class StudentMenuController : Controller
     {
+        private readonly ApplicationDataContext _context;
+
+        public StudentMenuController(ApplicationDataContext context)
+        {
+            _context = context;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
+
+        // GET:
+        public IActionResult ListStudents()
+        {
+            return View(_context.Student.ToList() );
+        }
+
     }
 }
---
+++ Rmit.Asr.Application/Rmit.Asr.Application/Migrations/20190125114852_new-database-studentmenu.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rmit.Asr.Application.Migrations
+{
+    public partial class newdatabasestudentmenu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
