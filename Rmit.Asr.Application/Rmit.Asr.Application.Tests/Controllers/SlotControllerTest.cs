d265e1226818e7aabfb822d1088a1c5bf20e4930
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-01-29 18:20:50 +1100

    Cleaned up slot controller tests and made them pass again.

--- Rmit.Asr.Application/Rmit.Asr.Application.Tests/Controllers/SlotControllerTest.cs
+++ Rmit.Asr.Application/Rmit.Asr.Application.Tests/Controllers/SlotControllerTest.cs
@@ -1,14 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Moq;
-using Rmit.Asr.Application.Controllers;
 using Rmit.Asr.Application.Models;
 using Rmit.Asr.Application.Models.ViewModels;
 using Xunit;
@@ -17,55 +11,25 @@ namespace Rmit.Asr.Application.Tests.Controllers
 {
     public class SlotControllerTest : ControllerBaseTest
     {
-        private readonly SlotController _controller;
-        private Staff _loggedInUser;
-
-        public SlotControllerTest()
-        {
-            _loggedInUser = new Staff
-            {
-                Id = "e12345",
-                StaffId = "e12345",
-                Email = "e12345@rmit.edu.au",
-                UserName = "e12345@rmit.edu.au"
-            };
-            var userPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new[]
-            {
-                new Claim(ClaimTypes.NameIdentifier, _loggedInUser.UserName)
-            }));
-            
-            var mockUserStore = new Mock<IUserStore<Staff>>();
-            mockUserStore.Setup(x => x.FindByIdAsync(It.IsAny<string>(), CancellationToken.None))
-                .ReturnsAsync(_loggedInUser);
-            
-            var mockUserManager = new UserManager<Staff>(mockUserStore.Object, null, null, null, null, null, null, null, null);
-            _controller = new SlotController(Context, mockUserManager)
-            {
-                ControllerContext =  new ControllerContext
-                {
-                    HttpContext = new DefaultHttpContext { User = userPrincipal }
-                }
-            };
-        }
-        
         [Fact]
         public async Task CreateSlot_WithValidParameters_ReturnSuccess()
         {
             // Arrange
+            UserLoggedIn(StaffUsername);
             var slot = new CreateSlot
             {
                 RoomId = "C",
-                StaffId = "e12345",
+                StaffId = StaffId,
                 StartTime = new DateTime(2019, 1, 1, 8, 0, 0)
             };
 
             // Act
-            IActionResult result = await _controller.Create(slot);
+            IActionResult result = await Controller.Create(slot);
 
             // Assert
             // No validation errors have occured
-            Assert.Empty(_controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage));
-            Assert.True(_controller.ModelState.IsValid);
+            Assert.Empty(Controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage));
+            Assert.True(Controller.ModelState.IsValid);
             
             // Controller redirected
             var viewResult = Assert.IsType<RedirectToActionResult>(result);
@@ -79,22 +43,23 @@ namespace Rmit.Asr.Application.Tests.Controllers
         public async Task CreateSlot_WithNonExistentRoom_ReturnSuccess()
         {
             // Arrange
+            UserLoggedIn(StaffUsername);
             var slot = new CreateSlot
             {
                 RoomId = "Z",
-                StaffId = "e12345",
+                StaffId = StaffId,
                 StartTime = new DateTime(2019, 1, 1, 8, 0, 0)
             };
 
             // Act
-            IActionResult result = await _controller.Create(slot);
+            IActionResult result = await Controller.Create(slot);
 
             // Assert
-            IEnumerable<string> errorMessages = _controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
+            IEnumerable<string> errorMessages = Controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
             
             // No validation errors have occured
             Assert.Contains(errorMessages, e => e == $"Room {slot.RoomId} does not exist.");
-            Assert.False(_controller.ModelState.IsValid);
+            Assert.False(Controller.ModelState.IsValid);
             
             // Controller returns a view
             Assert.IsType<ViewResult>(result);
@@ -107,10 +72,11 @@ namespace Rmit.Asr.Application.Tests.Controllers
         public async Task CreateSlot_WithOverMaximumRoomBooking_ReturnFail()
         {
             // Arrange
+            UserLoggedIn(StaffUsername);
             var slot = new CreateSlot
             {
                 RoomId = "A",
-                StaffId = "e12345",
+                StaffId = StaffId,
                 StartTime = new DateTime(2019, 1, 1, 9, 0, 0)
             };
 
@@ -118,13 +84,13 @@ namespace Rmit.Asr.Application.Tests.Controllers
                 new Slot
                 {
                     RoomId = "A",
-                    StaffId = "e12345",
+                    StaffId = StaffId,
                     StartTime = new DateTime(2019, 1, 1, 12, 0, 0)
                 },
                 new Slot
                 {
                     RoomId = "A",
-                    StaffId = "e12345",
+                    StaffId = StaffId,
                     StartTime = new DateTime(2019, 1, 1, 14, 0, 0)
                 }
             );
@@ -132,14 +98,14 @@ namespace Rmit.Asr.Application.Tests.Controllers
             Context.SaveChanges();
 
             // Act
-            IActionResult result = await _controller.Create(slot);
+            IActionResult result = await Controller.Create(slot);
 
             // Assert
-            IEnumerable<string> errorMessages = _controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
+            IEnumerable<string> errorMessages = Controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
             
             // No validation errors have occured
             Assert.Contains(errorMessages, e => e == $"Room {slot.RoomId} has reached a maximum booking of {Room.MaxRoomBookingPerDay} per day.");
-            Assert.False(_controller.ModelState.IsValid);
+            Assert.False(Controller.ModelState.IsValid);
             
             // Controller returns a view
             Assert.IsType<ViewResult>(result);
@@ -152,10 +118,11 @@ namespace Rmit.Asr.Application.Tests.Controllers
         public async Task CreateSlot_WithStaffOverMaximumBooking_ReturnFail()
         {
             // Arrange
+            UserLoggedIn(StaffUsername);
             var slot = new CreateSlot
             {
                 RoomId = "D",
-                StaffId = "e12345",
+                StaffId = StaffId,
                 StartTime = new DateTime(2019, 1, 1, 13, 0,  0)
             };
 
@@ -163,25 +130,25 @@ namespace Rmit.Asr.Application.Tests.Controllers
                 new Slot
                 {
                     RoomId = "A",
-                    StaffId = "e12345",
+                    StaffId = StaffId,
                     StartTime = new DateTime(2019, 1, 1, 12, 0, 0)
                 },
                 new Slot
                 {
                     RoomId = "A",
-                    StaffId = "e12345",
+                    StaffId = StaffId,
                     StartTime = new DateTime(2019, 1, 1, 14, 0, 0)
                 },
                 new Slot
                 {
                     RoomId = "B",
-                    StaffId = "e12345",
+                    StaffId = StaffId,
                     StartTime = new DateTime(2019, 1, 1, 10, 0, 0)
                 },
                 new Slot
                 {
                     RoomId = "B",
-                    StaffId = "e12345",
+                    StaffId = StaffId,
                     StartTime = new DateTime(2019, 1, 1, 9, 0, 0)
                 }
             );
@@ -189,14 +156,14 @@ namespace Rmit.Asr.Application.Tests.Controllers
             await Context.SaveChangesAsync();
 
             // Act
-            IActionResult result = await _controller.Create(slot);
+            IActionResult result = await Controller.Create(slot);
 
             // Assert
-            IEnumerable<string> errorMessages = _controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
+            IEnumerable<string> errorMessages = Controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
             
             // No validation errors have occured
-            Assert.Contains(errorMessages, e => e == $"Staff {_loggedInUser.StaffId} has a maximum of {Staff.MaxBookingPerDay} bookings at {slot.StartTime:dd-MM-yyyy}.");
-            Assert.False(_controller.ModelState.IsValid);
+            Assert.Contains(errorMessages, e => e == $"Staff {LoggedInStaff.StaffId} has a maximum of {Staff.MaxBookingPerDay} bookings at {slot.StartTime:dd-MM-yyyy}.");
+            Assert.False(Controller.ModelState.IsValid);
             
             // Controller returns a view
             Assert.IsType<ViewResult>(result);
@@ -209,13 +176,14 @@ namespace Rmit.Asr.Application.Tests.Controllers
         public async Task CreateSlot_WithStaffAlreadyTakenSlot_ReturnFail()
         {
             // Arrange
+            UserLoggedIn(StaffUsername);
             var slot = new CreateSlot
             {
                 RoomId = "A",
-                StaffId = "e12345",
+                StaffId = StaffId,
                 Staff = new Staff
                 {
-                    StaffId = "e12345"
+                    StaffId = StaffId
                 },
                 StartTime = new DateTime(2019, 1, 1, 12, 0, 0)
             };
@@ -236,14 +204,14 @@ namespace Rmit.Asr.Application.Tests.Controllers
             await Context.SaveChangesAsync();
 
             // Act
-            IActionResult result = await _controller.Create(slot);
+            IActionResult result = await Controller.Create(slot);
 
             // Assert
-            IEnumerable<string> errorMessages = _controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
+            IEnumerable<string> errorMessages = Controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
             
             // No validation errors have occured
             Assert.Contains(errorMessages, e => e == $"Staff e54321 has already taken slot at room {slot.RoomId} {slot.StartTime:dd-MM-yyyy H:mm}.");
-            Assert.False(_controller.ModelState.IsValid);
+            Assert.False(Controller.ModelState.IsValid);
             
             // Controller returns a view
             Assert.IsType<ViewResult>(result);
@@ -256,10 +224,11 @@ namespace Rmit.Asr.Application.Tests.Controllers
         public async Task CreateSlot_WithSlotAlreadyExists_ReturnFail()
         {
             // Arrange
+            UserLoggedIn(StaffUsername);
             var slot = new CreateSlot
             {
                 RoomId = "A",
-                StaffId = "e12345",
+                StaffId = StaffId,
                 StartTime = new DateTime(2019, 1, 1, 12, 0, 0)
             };
 
@@ -267,10 +236,10 @@ namespace Rmit.Asr.Application.Tests.Controllers
                 new Slot
                 {
                     RoomId = "A",
-                    StaffId = "e12345",
+                    StaffId = StaffId,
                     Staff = new Staff
                     {
-                        StaffId = "e12345"
+                        StaffId = StaffId
                     },
                     StartTime = new DateTime(2019, 1, 1, 12, 0, 0)
                 }
@@ -279,14 +248,14 @@ namespace Rmit.Asr.Application.Tests.Controllers
             await Context.SaveChangesAsync();
 
             // Act
-            IActionResult result = await _controller.Create(slot);
+            IActionResult result = await Controller.Create(slot);
 
             // Assert
-            IEnumerable<string> errorMessages = _controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
+            IEnumerable<string> errorMessages = Controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
             
             // No validation errors have occured
             Assert.Contains(errorMessages, e => e == $"Slot at room {slot.RoomId} {slot.StartTime:dd-MM-yyyy H:mm} already exists.");
-            Assert.False(_controller.ModelState.IsValid);
+            Assert.False(Controller.ModelState.IsValid);
             
             // Controller returns a view
             Assert.IsType<ViewResult>(result);
@@ -296,17 +265,18 @@ namespace Rmit.Asr.Application.Tests.Controllers
         public async Task CreateSlot_WithStaffAlreadyCreatedSlot_ReturnFail()
         {
             // Arrange
+            UserLoggedIn(StaffUsername);
             var slot = new CreateSlot
             {
                 RoomId = "D",
-                StaffId = "e12345",
+                StaffId = StaffId,
                 StartTime = new DateTime(2019, 1, 1, 13, 0, 0)
             };
 
             var createdSlot = new Slot
             {
                 RoomId = "A",
-                StaffId = "e12345",
+                StaffId = StaffId,
                 StartTime = new DateTime(2019, 1, 1, 13, 0, 0)
             };
 
@@ -315,14 +285,14 @@ namespace Rmit.Asr.Application.Tests.Controllers
             await Context.SaveChangesAsync();
 
             // Act
-            IActionResult result = await _controller.Create(slot);
+            IActionResult result = await Controller.Create(slot);
 
             // Assert
-            IEnumerable<string> errorMessages = _controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
+            IEnumerable<string> errorMessages = Controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
             
             // No validation errors have occured
             Assert.Contains(errorMessages, e => e == $"You have already created a slot at room {createdSlot.RoomId} {createdSlot.StartTime:dd-MM-yyyy H:mm}.");
-            Assert.False(_controller.ModelState.IsValid);
+            Assert.False(Controller.ModelState.IsValid);
             
             // Controller returns a view
             Assert.IsType<ViewResult>(result);
@@ -335,6 +305,7 @@ namespace Rmit.Asr.Application.Tests.Controllers
         public async Task RemoveSlot_WithStudentBookedInSlot_ReturnFail()
         {
             // Arrange
+            UserLoggedIn(StaffUsername);
             var slot = new RemoveSlot
             {
                 RoomId = "A",
@@ -344,7 +315,7 @@ namespace Rmit.Asr.Application.Tests.Controllers
             var createdSlot = new Slot
             {
                 RoomId = "A",
-                StaffId = "e12345",
+                StaffId = StaffId,
                 StudentId = "s1234567",
                 StartTime = new DateTime(2019, 1, 1, 13, 0, 0)
             };
@@ -354,14 +325,14 @@ namespace Rmit.Asr.Application.Tests.Controllers
             await Context.SaveChangesAsync();
 
             // Act
-            IActionResult result = await _controller.Remove(slot);
+            IActionResult result = await Controller.Remove(slot);
 
             // Assert
-            IEnumerable<string> errorMessages = _controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
+            IEnumerable<string> errorMessages = Controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
             
             // No validation errors have occured
             Assert.Contains(errorMessages, e => e == "Cannot remove slot as a student has been booked into it.");
-            Assert.False(_controller.ModelState.IsValid);
+            Assert.False(Controller.ModelState.IsValid);
             
             // Controller returns a view
             Assert.IsType<ViewResult>(result);
@@ -374,6 +345,7 @@ namespace Rmit.Asr.Application.Tests.Controllers
         public async Task RemoveSlot_WithANonExistentSlot_ReturnFail()
         {
             // Arrange
+            UserLoggedIn(StaffUsername);
             var slot = new RemoveSlot
             {
                 RoomId = "ASS",
@@ -383,7 +355,7 @@ namespace Rmit.Asr.Application.Tests.Controllers
             var createdSlot = new Slot
             {
                 RoomId = "A",
-                StaffId = "e12345",
+                StaffId = StaffId,
                 StudentId = "s1234567",
                 StartTime = new DateTime(2019, 1, 1, 13, 0, 0)
             };
@@ -393,14 +365,14 @@ namespace Rmit.Asr.Application.Tests.Controllers
             await Context.SaveChangesAsync();
 
             // Act
-            IActionResult result = await _controller.Remove(slot);
+            IActionResult result = await Controller.Remove(slot);
 
             // Assert
-            IEnumerable<string> errorMessages = _controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
+            IEnumerable<string> errorMessages = Controller.ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
             
             // No validation errors have occured
             Assert.Contains(errorMessages, e => e == $"Slot at room {slot.RoomId} {slot.StartTime:dd-MM-yyyy H:mm} does not exist.");
-            Assert.False(_controller.ModelState.IsValid);
+            Assert.False(Controller.ModelState.IsValid);
             
             // Controller returns a view
             Assert.IsType<ViewResult>(result);
