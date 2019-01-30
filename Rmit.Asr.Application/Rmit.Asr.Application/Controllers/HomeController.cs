c48f7d476f7adcb060668261f700f2abaadb8efc
shawnotaylor <shawnotaylor04@gmail.com>
2019-01-30 14:14:18 +1100

    added sitemap view and links

--- Rmit.Asr.Application/Rmit.Asr.Application/Controllers/HomeController.cs
+++ Rmit.Asr.Application/Rmit.Asr.Application/Controllers/HomeController.cs
@@ -37,6 +37,11 @@ namespace Rmit.Asr.Application.Controllers
             return View();
         }
 
+        public IActionResult Sitemap()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
