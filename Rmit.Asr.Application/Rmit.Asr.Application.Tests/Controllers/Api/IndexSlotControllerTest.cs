1f6a5eec21a33ba8c06674a581ef72cecbd50059
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-01-30 17:20:15 +1100

    Split out slot controller tests.

--- Rmit.Asr.Application/Rmit.Asr.Application.Tests/Controllers/Api/IndexSlotControllerTest.cs
+++ Rmit.Asr.Application/Rmit.Asr.Application.Tests/Controllers/Api/IndexSlotControllerTest.cs
@@ -10,7 +10,7 @@ using Xunit;
 
 namespace Rmit.Asr.Application.Tests.Controllers.Api
 {
-    public class SlotControllerTest : ControllerBaseTest
+    public class IndexSlotControllerTest : ControllerBaseTest
     {
         [Fact]
         public async void IndexSlot_WithValidParameters_ReturnSlots()
@@ -191,72 +191,6 @@ namespace Rmit.Asr.Application.Tests.Controllers.Api
             // Assert
             Assert.IsAssignableFrom<JsonResult>(result);
             Assert.Equal("Slot does not exist.", result.Value.Message);
-            Assert.Equal((int) HttpStatusCode.NotFound, result.StatusCode);
-            
-            Assert.True(Context.Slot.Any(s => s.RoomId == slot.RoomId && s.StartTime == slot.StartTime && s.StudentId == slot.StudentId));
-        }
-        
-        [Fact]
-        public async void PutSlot_WithNonExistentRoom_ReturnNotFound()
-        {
-            // Arrange
-            var slot = new Slot
-            {
-                RoomId = "A",
-                StaffId = StaffId,
-                StudentId = StudentId,
-                StartTime = new DateTime(2019, 1, 1, 13, 0, 0)
-            };
-
-            Context.Slot.Add(slot);
-
-            await Context.SaveChangesAsync();
-
-            dynamic values = new ExpandoObject();
-            values.StudentId = new ExpandoObject();
-            values.StartTime = new ExpandoObject();
-            values.StudentId.Value = slot.StudentId;
-            values.StartTime.Value = slot.StartTime.ToString();
-
-            // Act
-            dynamic result = ApiSlotController.Put("Z", values);
-
-            // Assert
-            Assert.IsAssignableFrom<JsonResult>(result);
-            Assert.Equal("Room does not exist.", result.Value.Message);
-            Assert.Equal((int) HttpStatusCode.NotFound, result.StatusCode);
-            
-            Assert.True(Context.Slot.Any(s => s.RoomId == slot.RoomId && s.StartTime == slot.StartTime && s.StudentId == slot.StudentId));
-        }
-        
-        [Fact]
-        public async void PutSlot_WithNonExistentStudent_ReturnNotFound()
-        {
-            // Arrange
-            var slot = new Slot
-            {
-                RoomId = "A",
-                StaffId = StaffId,
-                StudentId = StudentId,
-                StartTime = new DateTime(2019, 1, 1, 13, 0, 0)
-            };
-
-            Context.Slot.Add(slot);
-
-            await Context.SaveChangesAsync();
-
-            dynamic values = new ExpandoObject();
-            values.StudentId = new ExpandoObject();
-            values.StartTime = new ExpandoObject();
-            values.StudentId.Value = "asd";
-            values.StartTime.Value = slot.StartTime.ToString();
-
-            // Act
-            dynamic result = ApiSlotController.Put(slot.RoomId, values);
-
-            // Assert
-            Assert.IsAssignableFrom<JsonResult>(result);
-            Assert.Equal("Student does not exist.", result.Value.Message);
             Assert.Equal((int) HttpStatusCode.NotFound, result.StatusCode);
             
             Assert.True(Context.Slot.Any(s => s.RoomId == slot.RoomId && s.StartTime == slot.StartTime && s.StudentId == slot.StudentId));
